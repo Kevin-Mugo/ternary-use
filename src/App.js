5684e07d248b183690cef25c91ebec614ffb188b
ANZY KENYA <mugokevin900@gmail.com>
2022-12-26 13:45:57 +0300

    #1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App(){
+
+  const isGoingOut = false;
+
+
+
+  return(
+    <div className = "state">
+      <h1 className = "state--title">Do i feel like going out tonight?</h1>
+      <div className = "state--value">
+        
+       <h1>{isGoingOut === true ? "yes" : "No"}</h1>
     </div>
-  );
-}
+    </div>
+  )
 
-export default App;
+};
