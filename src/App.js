519e5c396081a970c698c3385ca373e7e1c50dc8
ANZY KENYA <mugokevin900@gmail.com>
2022-12-30 11:22:12 +0300

    #2

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import './App.css'
 
 export default function App(){
 
-  const isGoingOut = false;
-
+  const [isGoingOut , setIsGoingOUT]  = React.useState(true);
+function Set(){
+  setIsGoingOUT(prevState => !prevState);
+};
 
 
   return(
     <div className = "state">
       <h1 className = "state--title">Do i feel like going out tonight?</h1>
-      <div className = "state--value">
+      <div onClick={Set} className = "state--value">
         
-       <h1>{isGoingOut === true ? "yes" : "No"}</h1>
+       <h1>{isGoingOut ? "Yes" : "No"}</h1>
     </div>
     </div>
   )
